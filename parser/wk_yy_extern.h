7b943673de7a95760b67fc8ddaff11c470a8a0ad
skylerlee <skyler.ac.lee@gmail.com>
2021-02-08 16:21:00 +0000

    feat: add externs

---
+++ parser/wk_yy_extern.h
@@ -0,0 +1,16 @@
+#ifndef WANKEL_PARSER_WK_YY_EXTERN_H_
+#define WANKEL_PARSER_WK_YY_EXTERN_H_
+
+#include <stdio.h>
+
+// typedef int* yyscan_t;
+// typedef int* yyparser_t;
+// typedef int YYSTYPE;
+
+// extern int yylex_init(yyscan_t* scan);
+// extern int yylex_destroy(yyscan_t scan);
+// extern int yylex(YYSTYPE* val, yyscan_t scan);
+// extern void yyset_in(FILE* in_str, yyscan_t scan);
+// extern void yyerror(yyparser_t parser, const char* message);
+
+#endif  // WANKEL_PARSER_WK_YY_EXTERN_H_
